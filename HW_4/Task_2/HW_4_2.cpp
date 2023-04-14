5185f21a3b7ebf80d6cc4704439fd855fe36a487
KarpenkoBM <karpenko.bm@phystech.edu>
2023-04-14 16:31:12 +0300

    Changed second task in 4-th homework

--- HW_4/Task_2/HW_4_2.cpp
+++ HW_4/Task_2/HW_4_2.cpp
@@ -10,34 +10,29 @@
 #include<future>
 #include <mutex>
 
-std::mutex door;
-
 template<typename Iter, typename Func>
-
-void parallel_for_each( Iter begin, Iter end, Func f)
-{
-
-    unsigned long const length = std::distance(begin, end);
-    if (length == 0 ) return;
-    const int numberOfThreads = std::thread::hardware_concurrency();
-    unsigned long const MinElementsPerThread = 50;
-    if (length < (2 * MinElementsPerThread))
-        {
-            door.lock();
-            std::for_each(begin, end, f);
-            door.unlock();
-        }
-    else
-        {
-            Iter  mid_point = begin + length / 2;
-            std::future<void> first_half = std::async(&parallel_for_each<Iter,Func>,begin, mid_point, f);
-            parallel_for_each(mid_point, end, f);
-        }
+int block_for_each(Iter begin, Iter end, Func f) {
+    std::for_each(begin, end, f);
 }
-
+template<typename Iter, typename Func>
+void parallel_for_each (Iter begin, Iter end, Func f)
+{
+    auto num_threads = std::thread::hardware_concurrency();
+    auto block_size = std::distance(begin, end) / num_threads;
+    std::vector<std::future<int>> threads;
+    for (auto i = 0u; i + 1 < num_threads; i++)
+        {
+            threads.push_back(std::async(std::launch::async, &block_for_each<Iter, Func>, std::next(begin, i * block_size), std::next(begin, (i + 1) * block_size), f));
+        }
+    threads.push_back(std::async(std::launch::async, &block_for_each<Iter, Func>, std::next(begin, (num_threads - 1) * block_size), end, f));
+    for(auto& it : threads)
+    {
+        it.wait();
+    }
+}
 int main()
 {
-    size_t size = 1000;
+    size_t size = 10000;
     //std::cin >>size;
     std::vector<int>  sequence_1(size);
     for (auto i = 0; i < size; i++) sequence_1[i] = i;
@@ -46,4 +41,8 @@ int main()
     parallel_for_each(sequence_1.begin(),sequence_1.end(), [](int &n){n *= n;});
     std::cout<<std::endl;
     parallel_for_each(sequence_1.begin(),sequence_1.end(), print );
+    //std::for_each(sequence_1.begin(),sequence_1.end(), print);
+    //std::for_each(sequence_1.begin(),sequence_1.end(), [](int &n){n *= n;});
+     //std::for_each(sequence_1.begin(),sequence_1.end(), print);
+
 }
